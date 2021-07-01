68223e60113a8271b3cb0f3df86bf9de2b66486e
mjlindskog <lindskogmarkj@gmail.com>
2021-06-30 21:43:01 -0400

    updated main README.md, fix badges and table of contents in js, add comments

--- README.md
+++ README.md
@@ -1,11 +1,69 @@
-{
-	"title": "test",
-	"description": "this is a test",
-	"installation": "i amtesting this",
-	"usage": "i am",
-	"contribution": "they cant",
-	"tests": "dont",
-	"license": "mit",
-	"username": "mjlindskog",
-	"email": "mjlindskog@hotmail.com"
-}
+# README Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+- Built so user can generate basic README.md by answering a few questions through npm inquirer
+- README docuaments are an important part of any repo and git
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Open console:
+
+If no package.json file is present, type the command: `npm init` ,then: `npm install inquirer`
+
+If package.json file is present then type the command: `npm install`
+
+Below is video demonstrating the markdown generation:
+
+https://drive.google.com/file/d/16qoigEXETBG6rqtcLhvu257zeHR2FH6n/view?usp=sharing
+
+## Usage
+
+Once installation is complete run: `node index.js`
+
+![GIF of md document generated through the console](./img/README.md_Generator.gif)
+
+## Credits
+
+https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba - info on license badges
+
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide - helped with the README guidelines
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Mark Lindskog
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Features
+
+- prompts will ask questions to help guide user to create basic README
+- File will be generated/ overwritten if one already exists in generated folder
+- Links to github, email, and license will be present in generated README
--- index.js
+++ index.js
@@ -1,102 +1,99 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
+// imports inquirer and fs
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
     .prompt([
         {
+            // Top header that will appear on README
             type:'input',
             name:'title',
             message: 'What would you like to title this project?'
         },
         {
+            // user can write desrcition of project here
             type:'input',
             name: 'description',
             message: 'Please write a brief description of your project.'
         },
         {
+            // installation instructions such as code snippets or downloading procedures will be written here
             type:'input',
             name: 'installation',
             message: 'Write a brief description of any installation instructions associated with your project.'
         },
         {
+            // Tell users how to use the project
             type:'input',
             name: 'usage',
             message: 'Write a brief description of your project will be used.'
         },
         {
+            // describe how other users can contribute
             type:'input',
             name: 'contribution',
             message: 'How can others contribute to your project.'
         },
         {
+            // tests to run 
             type:'input',
             name: 'tests',
             message: 'Please write how to test your project.'
         },
         {
+            // choose from a list of licenses
             type: 'list',
             name: 'license',
             message: 'Please choose a license for your project.',
             choices: ['Apache 2.0', 'MIT', 'Mozilla Public License 2.0', 'GNU GPL v3', 'BSD 2-Clause', 'Unlicense']
         },
         {
+            // if inputted correctly will later allow link to be created to Github project
             type:'input',
             name: 'username',
             message: 'Please enter your github username.'
         },
         {
+            // if inputted correctly will later allow email to be sent to user
             type:'input',
             name: 'email',
             message: 'Please enter your email.'
         },
     ])
     .then((data) => {
+        // redirects generated README.md to a different folder to prevent overwrite of original README.md
         const filename = './generated/README.md';
 
         fs.writeFile(filename, writeMd(data), (err) =>
+            // error will be thrown back if unsuccessful
             err ? console.log(err) : console.log('README successfully generated!')
         );
     });
 
+// writes markdown document following basic README format presented in acceptance criteria
 function writeMd(data) {
     return `# ${data.title}
-  [![License](https://img.shields.io/badge/license-${data.license}-informational)](https://shields.io)
-  ## Table of Contents 
-  - [Description](#description)
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Contribution](#contribution)
-  - [Testing](#testing)
-  - [Additional Info](#additional-info)
-  ## Description:
-  ${data.description}
-  ## Installation:
-  ${data.installation}
-  ## Usage:
-  ${data.usage}
-  ## License:
-  ${data.license}
-  ## Contribution:
-  ${data.contribution}
-  ## Testing:
-  ${data.tests}
-  ## Questions:
-  - Github: [${data.github}](https://github.com/${data.username})
-  - Email: ${data.email}`;
+    [![License](https://img.shields.io/badge/license-${data.license}-informational)](https://shields.io)
+    ## Table of Contents 
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contribution](#contribution)
+    - [Testing](#testing)
+    - [Questions](#questions)
+    ## Description:
+    ${data.description}
+    ## Installation:
+    ${data.installation}
+    ## Usage:
+    ${data.usage}
+    ## License:
+    ${data.license}
+    ## Contribution:
+    ${data.contribution}
+    ## Testing:
+    ${data.tests}
+    ## Questions:
+    - Github: [${data.username}](https://github.com/${data.username})
+    - Email: ${data.email}`;
 }
